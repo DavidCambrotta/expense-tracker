a5e9df19f80c064284d50c51f0e0da641418c1e4
DavidCambrotta <cambrotta@gmail.com>
2025-10-09 17:01:56 +0100

    update main to choose from CLI or GUI

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from backend.database import init_db
 from frontend.cli import menu
+from frontend.gui import ExpenseTracker
+from PySide6.QtWidgets import QApplication
+import sys
 
 if __name__ == "__main__":
     init_db()
-    menu()
+
+    # Ask user which mode to run
+    mode = input("Choose mode: (1) CLI  (2) GUI → ").strip()
+
+    if mode == "1":
+        menu()
+    elif mode == "2":
+        app = QApplication(sys.argv)
+        window = ExpenseTracker()
+        window.show()
+        sys.exit(app.exec())
+    else:
+        print("Invalid option. Exiting.")
